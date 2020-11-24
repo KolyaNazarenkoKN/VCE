761a8023fa48d946fd6bfd3cfab5d2bd10914e85
lilyakaras <lilyca.hrupa@gmail.com>
2020-11-24 10:54:31 +0200

    Commit

---
+++ VCE Server/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCE_Server
+{
+   public  class Question
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Difficalt { get; set; }
+        public int IdTest { get; set; }
+    }
+}
---
+++ VCE Server/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCE_Server
+{
+   public class Test
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
