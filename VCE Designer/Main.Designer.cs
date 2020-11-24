99066486acc801e96691d4f3075d23a23770dd96
Naza_uhd7@student.itstep.org <Naza_uhd7@student.itstep.org>
2020-11-24 16:43:03 +0200

    Commit

--- VCE Designer/Main.Designer.cs
+++ VCE Designer/Main.Designer.cs
@@ -260,6 +260,7 @@
             this.button3.TabIndex = 15;
             this.button3.Text = "Зберегти зміни";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
--- VCE Designer/Main.cs
+++ VCE Designer/Main.cs
@@ -39,7 +39,7 @@ namespace VCE_Designer
             {
                 formatter.Serialize(fs, body);
             }
-            MessageBox.Show("Файл збережен" ,"Успішно",
+            MessageBox.Show("Файл збережений!", "Успішно",
                 MessageBoxButtons.OK,
                 MessageBoxIcon.Information);
 
@@ -53,22 +53,39 @@ namespace VCE_Designer
                 return;
             string filename = openFileDialog1.FileName;
             XmlSerializer formatter = new XmlSerializer(typeof(Body));
-            using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate))
+            XElement xml = XElement.Load(openFileDialog1.OpenFile());
+            var Author = xml.Descendants("Author").Select(t => t.Value);
+            foreach (var element in Author)
             {
-                Body newBody = (Body)formatter.Deserialize(fs);
-              
+                textBox1.Text = element;
             }
-            MessageBox.Show("Файл відкрито", "Успішно!",
+            var TestName = xml.Descendants("TestName").Select(t => t.Value);
+            foreach (var element in TestName)
+            {
+                textBox2.Text = element;
+            }
+            var NameQuestion = xml.Descendants("NameQuestion").Select(t => t.Value);
+            foreach (var element in NameQuestion)
+            {
+                textBox4.Text = element;
+            }
+            var description = xml.Descendants("description").Select(t => t.Value);
+            foreach (var element in description)
+            {
+                textBox3.Text = element;
+            }
+            MessageBox.Show("Файл відкрито!", "Успішно",
                 MessageBoxButtons.OK,
-                MessageBoxIcon.Information);
+                MessageBoxIcon.Information);    
+     
         }
-
+    
         private void вихідToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
-        
+
 
         private void button_ok_Click(object sender, EventArgs e)
         {
@@ -89,7 +106,7 @@ namespace VCE_Designer
 
         private void button_nextquestion_Click(object sender, EventArgs e)
         {
-        
+
             string NameQuestion = textBox_NameQuestion.Text;
             string Description = textBox_Description.Text;
             Question question = new Question();
@@ -103,16 +120,39 @@ namespace VCE_Designer
             textBox_Description.Text = "";
         }
 
-        
+
         private void button1_Click(object sender, EventArgs e)
         {
             string Description = textBox_Description.Text;
             Answer answer = new Answer();
-            answer.Description = "Hello";
+            answer.Description = Description;
             question.Answer.Add(answer);
             textBox_Description.Text = "";
         }
 
-       
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            string author = textBox_nameauthor.Text;
+            string NameTest = textBox_TestName.Text;
+            string Namequestion = textBox_NameQuestion.Text;
+            string Description = textBox_Description.Text;
+            body.Author = author;
+            body.TestName = NameTest;
+            question.NameQuestion = Namequestion;
+            body.Question.Add(question);
+            saveFileDialog1.Filter = "XML files(*.xml)|*.xml|All files(*.*)|*.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = saveFileDialog1.FileName;
+            XmlSerializer formatter = new XmlSerializer(typeof(Body));
+            using (FileStream fs = new FileStream(filename, FileMode.CreateNew))
+            {
+                formatter.Deserialize(fs);
+            }
+            MessageBox.Show("Файл змінений!", "Успішно",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
     }
 }
