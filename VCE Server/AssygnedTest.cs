f843ca2caa0f52c0a0c1b1e6312ab36386e069a3
lilyakaras <lilyca.hrupa@gmail.com>
2020-11-24 11:07:20 +0200

    Commit

---
+++ VCE Server/AssygnedTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCE_Server
+{
+    public class AssygnedTest
+    {
+        public int Id { get; set; }
+        public int IdGroup { get; set; }
+        public int IdTest { get; set; }
+    }
+}
